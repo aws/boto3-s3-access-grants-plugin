d5410df226fb4dce693f13598721f2b9fc8d85bf
Shiva Kumar Mukkapati <mshvkmr@amazon.com>
2025-07-23 16:28:57 -0400

    updating tests

--- tests/integration/test_setup.py
+++ tests/integration/test_setup.py
@@ -8,7 +8,7 @@ from botocore.exceptions import ClientError
 
 
 class SetupIntegrationTests:
-    test_account = "020383678445"   # Set your account number here
+    test_account = ""   # Set your account number here
     iam_role_name = 'aws-s3-access-grants-sdk-plugin-integration-role'
     iam_client = boto3.client('iam')
     region = 'us-west-1'
